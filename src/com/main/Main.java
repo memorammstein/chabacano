234614e3baa89f7a4bf04e8e5cf98cd97f55fd0f
Guillermo Iván Jiménez Ruvalcaba <engel_live_aus_rammsteinland@hotmail.com>
2015-10-23 09:36:25 -0500

    made it eclipse project

---
+++ src/com/main/Main.java
@@ -0,0 +1,16 @@
+package com.main;
+
+import com.compis.M_Table;
+import com.compis.Symbol;
+import com.compis.Transition;
+import com.compis.analyzer.PredictiveSyntacticAnalyzer;
+
+public class Main {
+	public static void main(String[] args) {
+		PredictiveSyntacticAnalyzer psa = null;
+		M_Table m = null;
+		Symbol s = null;
+		Transition t = null;
+		return;
+	}
+}
